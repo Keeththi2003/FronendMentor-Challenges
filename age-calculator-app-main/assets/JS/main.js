8d4750a1d68dccfeea3372c8445c8fdab8227f59
Keeththi2003 <keeththi2003@gmail.com>
2024-02-28 12:16:27 +0530

    Some work in script.js

---
+++ age-calculator-app-main/assets/JS/main.js
@@ -0,0 +1,7 @@
+const inputDay = document.getElementById("b-day");
+const inputMonth = document.getElementById("b-month");
+const inputYear = document.getElementById("b-year");
+const outputDay = document.getElementById("a-day");
+const outputMonth = document.getElementById("a-month");
+const outputYear = document.getElementById("a-year");
+
